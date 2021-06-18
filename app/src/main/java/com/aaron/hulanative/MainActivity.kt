2992218b1c0518b6022c2c831c991034a2669ee4
aaron <lizengyun@mampod.com>
2021-06-18 09:57:52 +0800

    five tab bar menu configure

--- app/src/main/java/com/aaron/hulanative/MainActivity.kt
+++ app/src/main/java/com/aaron/hulanative/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_recommend, R.id.navigation_discover,R.id.navigation_mine))
+                R.id.navigation_home, R.id.navigation_recommend,R.id.nav_topic_fragment, R.id.navigation_discover,R.id.nav_profile_fragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/com/aaron/hulanative/ui/mine/ProfileFragment.kt
@@ -0,0 +1,19 @@
+package com.aaron.hulanative.ui.mine
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aaron.hulanative.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
